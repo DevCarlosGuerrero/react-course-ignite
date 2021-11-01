46709ba2fdfb85e75cdc07de3e24956e5d6e5128
Carlos Guerrero <carlos.guerrero292@hotmail.com>
2021-11-01 09:02:20 -0400

    Create and import data to Game Cards.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Home from "./pages/home";
 function App() {
   return (
     <div className="App">
-      <h1>Hello Ignite!</h1>
       <Home />
     </div>
   );
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,19 +1,63 @@
 import { useEffect } from "react";
 
 // Redux
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { loadGames } from "../actions/gamesAction";
 
+//Styles
+import styled from "styled-components";
+
+// Components
+import Game from "../components/Game";
+
+// Animations
+import { motion } from "framer-motion";
+
 const Home = () => {
+  // Fetch Games
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(loadGames());
-  });
+  }, [dispatch]);
+
+  // Retreive the Data
+  const { popularGames, upcomingGames, newGames } = useSelector(
+    (state) => state.games
+  );
+
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <GameList>
+      <h2>Upcoming Games</h2>
+      <Games>
+        {upcomingGames &&
+          upcomingGames.map((game) => (
+            <Game
+              name={game.name}
+              released={game.released}
+              id={game.id}
+              image={game.background_image}
+              key={game.id}
+            />
+          ))}
+      </Games>
+    </GameList>
   );
 };
+
+const GameList = styled(motion.div)`
+  padding: 0rem 5rem;
+
+  h2 {
+    padding: 5rem 0rem;
+  }
+`;
+
+const Games = styled(motion.div)`
+  min-height: 80vh;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(500px, 1fr));
+  grid-column-gap: 3rem;
+  grid-row-gap: 5rem;
+`;
 
 export default Home;
