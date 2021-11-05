debeb78469c81048077d31b9cafd1159fbe396db
Carlos Guerrero <carlos.guerrero292@hotmail.com>
2021-11-05 08:37:55 -0400

    Implement global styles, and additional games

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 // Components
+import GlobalStyles from "./components/GlobalStyles";
+
 // Pages
 import Home from "./pages/home";
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyles />
       <Home />
     </div>
   );
--- src/pages/home.js
+++ src/pages/home.js
@@ -40,6 +40,32 @@ const Home = () => {
             />
           ))}
       </Games>
+      <h2>Pouplar Games</h2>
+      <Games>
+        {popularGames &&
+          popularGames.map((game) => (
+            <Game
+              name={game.name}
+              released={game.released}
+              id={game.id}
+              image={game.background_image}
+              key={game.id}
+            />
+          ))}
+      </Games>
+      <h2>New Games</h2>
+      <Games>
+        {newGames &&
+          newGames.map((game) => (
+            <Game
+              name={game.name}
+              released={game.released}
+              id={game.id}
+              image={game.background_image}
+              key={game.id}
+            />
+          ))}
+      </Games>
     </GameList>
   );
 };
